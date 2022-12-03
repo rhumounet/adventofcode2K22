59a576e4c2bb944887e9d6a31f4aa1e91b44d277
rhumounet <ples.arthur@gmail.com>
2022-12-03 07:06:57 +0100

    ♫ C'EST LE JOUR 3 ♫

--- AdventOfCode2K22/Days/03/Part1.cs
+++ AdventOfCode2K22/Days/03/Part1.cs
@@ -4,8 +4,17 @@ public class Part1 : AbstractSolver
 {
     internal override async Task<string> CoreSolve(StreamReader reader) 
     {
+        var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         var content = await reader.ReadToEndAsync();
-        //Fais des trucs batard
-        return content;
+        var rucksacks = content.Split("\r\n");
+        var getPriority = (char c) => {
+            return alphabet.IndexOf(c) + 1;
+        };
+        var compartments = rucksacks.Select(r => r.Chunk(r.Length / 2));
+        var totalPriorities = compartments
+            .Sum(c => getPriority(c.ElementAt(0)
+                .Intersect(c.ElementAt(1)).ElementAt(0)));
+
+        return totalPriorities.ToString();
     }
 }
--- AdventOfCode2K22/Days/03/Part2.cs
+++ AdventOfCode2K22/Days/03/Part2.cs
@@ -1,11 +1,29 @@
 namespace Day3;
 
-public class Part2 : AbstractSolver 
+public class Part2 : AbstractSolver
 {
-    internal override async Task<string> CoreSolve(StreamReader reader) 
+    internal override async Task<string> CoreSolve(StreamReader reader)
     {
+        var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         var content = await reader.ReadToEndAsync();
-        //Fais des trucs batard
-        return content;
+        var i = 0;
+        var rucksacks = content.Split("\r\n").Aggregate((a, b) =>
+        {
+            return $"{a}{(i++ != 0 && i % 3 == 0 ? "@" : "+")}{b}";
+        });
+        var getPriority = (char c) =>
+        {
+            return alphabet.IndexOf(c) + 1;
+        };
+        var totalPriorities = rucksacks.Split('@').Sum(g =>
+        {
+            var grouped = g.Split('+');
+            return getPriority(grouped.ElementAt(0)
+                .Intersect(grouped.ElementAt(1))
+                .Intersect(grouped.ElementAt(2))
+                .ElementAt(0));
+        });
+
+        return totalPriorities.ToString();
     }
 }
