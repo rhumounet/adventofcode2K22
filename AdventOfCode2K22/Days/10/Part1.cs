e624ff93ee73264b68ea94caded82f4df1809b97
rhumounet <ples.arthur@gmail.com>
2022-12-10 10:37:03 +0100

    Day 10, je suis maintenant un afficheur CRT

--- AdventOfCode2K22/Days/10/Part1.cs
+++ AdventOfCode2K22/Days/10/Part1.cs
@@ -1,11 +1,39 @@
+using System.Text.RegularExpressions;
+
 namespace Day10;
 
-public class Part1 : AbstractSolver 
+public class Part1 : AbstractSolver
 {
-    internal override async Task<string> CoreSolve(StreamReader reader) 
+    internal override async Task<string> CoreSolve(StreamReader reader)
     {
         var content = await reader.ReadToEndAsync();
-        //Fais des trucs batard
-        return content;
+        var instructions = content.Split("\r\n");
+        var nooprgx = new Regex("^noop");
+        var addxrgx = new Regex(@"^addx (-)?(\d*)");
+        var cycles = new Queue<int>();
+        var list = new List<int>();
+        int register = 1;
+        Match match;
+        int i = 0;
+        while (cycles.Any() || i <= instructions.Length)
+        {
+            if (i < instructions.Length)
+                if ((match = nooprgx.Match(instructions[i])).Success)
+                {
+                    cycles.Enqueue(0);
+                    // noop
+                }
+                else if ((match = addxrgx.Match(instructions[i])).Success)
+                {
+                    cycles.Enqueue(0);
+                    cycles.Enqueue(int.Parse($"{match.Groups[1].Value}{match.Groups[2].Value}"));
+                }
+            if ((i + 1) % 40 == 20)
+                list.Add((i + 1) * register);
+            if (cycles.Any())
+                register += cycles.Dequeue();
+            i++;
+        }
+        return $"{list.Sum()}";
     }
 }
--- AdventOfCode2K22/Days/10/Part2.cs
+++ AdventOfCode2K22/Days/10/Part2.cs
@@ -1,11 +1,52 @@
+using System.Text.RegularExpressions;
+
 namespace Day10;
 
-public class Part2 : AbstractSolver 
+public class Part2 : AbstractSolver
 {
-    internal override async Task<string> CoreSolve(StreamReader reader) 
+    internal override async Task<string> CoreSolve(StreamReader reader)
     {
         var content = await reader.ReadToEndAsync();
-        //Fais des trucs batard
-        return content;
+        var instructions = content.Split("\r\n");
+        var nooprgx = new Regex("^noop");
+        var addxrgx = new Regex(@"^addx (-)?(\d*)");
+        var cycles = new Queue<int>();
+        var list = new List<int>();
+        int register = 1;
+        int lineIndex = 0;
+        Match match;
+        int i = 0;
+        var stringBuilder = new StringBuilder();
+        while (cycles.Any() || i <= instructions.Length)
+        {
+            if (i < instructions.Length)
+                if ((match = nooprgx.Match(instructions[i])).Success)
+                {
+                    cycles.Enqueue(0);
+                    // noop
+                }
+                else if ((match = addxrgx.Match(instructions[i])).Success)
+                {
+                    cycles.Enqueue(0);
+                    cycles.Enqueue(int.Parse($"{match.Groups[1].Value}{match.Groups[2].Value}"));
+                }
+            char toDraw;
+            if (lineIndex <= register + 1 && lineIndex >= register - 1)
+                toDraw = '#';
+            else
+                toDraw = '.';
+            if (lineIndex == 0)
+                stringBuilder.AppendLine();
+            stringBuilder.Append(toDraw);
+            if ((i + 1) % 40 == 20)
+                list.Add((i + 1) * register);
+            if (cycles.Any())
+                register += cycles.Dequeue();
+
+            i++;
+            lineIndex += lineIndex == 39 ? -39 : 1;
+        }
+        Console.Write(stringBuilder.ToString());
+        return $"{list.Sum()}";
     }
 }
